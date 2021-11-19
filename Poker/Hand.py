856998c80ed61390809f7d584f77b9bd22b5c28d
Christoffer <dragonrooster@hotmail.com>
2021-11-19 15:03:22 +0100

    asd

--- Poker/Hand.py
+++ Poker/Hand.py
@@ -1,14 +1,9 @@
-from .Deck import DeckFactory
-from .Player import Player
+from Deck import DeckFactory
+from Player import Player
 from typing import List
 
-
 class LHEHand:
-    playerHands = []
-    flop = []
-    turn = []
-    river = []
-    pot = 0
+
 
     def PreFlop(self):
         for player in self.playersInHand:
@@ -16,14 +11,34 @@ class LHEHand:
 
         self.playersInHand[self.bigBlindIndex].take_money(self.bigBlind)
         self.playersInHand[self.bigBlindIndex - 1 % len(self.playersInHand)].take_money(self.smallBlind)
+        while (len(
+                    filter(
+                        lambda playerIndex, round, numberOfRaises, action :
+                        round == self.round and (action == 0 or action == 1),
+                        self.bettingHistory
+                    )
+                ) > (len(self.playersInHand)-1)):
+
+
+
+
 
     def __init__(self, bigBlind: int, bigBlindIndex: int, playersInHand: List[Player]):
+        #Params
         self.bigBlind = bigBlind
         self.smallBlind = bigBlind / 2
         self.bigBlindIndex = bigBlindIndex
         self.playersInHand = playersInHand
+
+        #Initialize vars
+        self.bettingHistory = []  # (playerIndex, round, numberOfRaises, action)
+        self.playerHands = []
+        self.flop = []
+        self.turn = []
+        self.river = []
+        self.round = 0
+        self.pot = 0
         self.deckFactory = DeckFactory(2)
         self.deck = self.deckFactory.create_deck()
 
         self.PreFlop()
-
