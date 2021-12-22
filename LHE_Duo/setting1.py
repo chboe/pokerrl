1a8b62e2a6a105ca36db17308064f65688a0345f
majegit <casper@majgaard.dk>
2021-12-22 20:33:36 +0100

    Started learning LHE_Duo with these parameters

--- LHE_Duo/setting1.py
+++ LHE_Duo/setting1.py
@@ -9,13 +9,13 @@ from Agents.Random_Agent import Random_Agent
 import time
 
 
-SAVE_INTERVAL = 2500
+SAVE_INTERVAL = 50_000
 MRL_SIZE = 500_000
-MSL_SIZE = 1_500_000
-RL_LR = 0.1
-SL_LR = 0.01
+MSL_SIZE = 2_500_000
+RL_LR = 0.01
+SL_LR = 0.001
 BATCH_SIZE = 256
-TARGET_POLICY_UPDATE_INTERVAL = 1000
+TARGET_POLICY_UPDATE_INTERVAL = 2500
 ANTICIPATORY_PARAM = 0.1
 EPS = 0.15
 EPS_DECAY = 3_000_000
@@ -48,10 +48,10 @@ while(True):
         print("Time taken:", time_passed)
         for i, p in enumerate(players_in):
             print(f'\nPlayer id={p.id}, total winnings={p.total_winnings}, average last 100 = {sum(p.last100)/len(p.last100)}')
-            steps_taken = p.agent.step_count - previous_steps[i]
+            steps_taken = p.agent.update_count - previous_steps[i]
             print(f'Player id={p.id} took steps {steps_taken}')
             print(f'Average seconds per step is {time_passed / steps_taken}')
-            previous_steps[i] = p.agent.step_count
+            previous_steps[i] = p.agent.update_count
         before = now
     
     
